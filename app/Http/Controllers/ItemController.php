0aabdffe3ea4c0332902c5e5e3006a8f1622556c
VenSnow <timur333t@yandex.ru>
2021-10-11 16:50:41 +0500

    Simple Finished

---
+++ app/Http/Controllers/ItemController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Item;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class ItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Item::orderByDesc('created_at')->get();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return Item
+     */
+    public function store(Request $request)
+    {
+        $newItem = new Item;
+        $newItem->name = $request->item['name'];
+        $newItem->save();
+
+        return $newItem;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $existingItem = Item::findOrFail($id);
+        $existingItem->completed = $request->item['completed'] ? true : false;
+        $existingItem->completed_at = $request->item['completed'] ? Carbon::now() : null;
+        $existingItem->save();
+        return $existingItem;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return string
+     */
+    public function destroy($id)
+    {
+        $existingItem = Item::findOrFail($id);
+        $existingItem->delete();
+        return "Item successfully deleted";
+    }
+}
